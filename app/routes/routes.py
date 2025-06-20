a973a62bb9e53a7c58de4bae1fb5fb19cc671875
Bhuvijain230 <bhuvijain230@gmail.com>
2025-06-20 14:30:56 +0530

    Initial commit

---
+++ app/routes/routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+from ..db import get_connection
+
+bp = Blueprint('routes', __name__)
+
+@bp.route('/medicines')
+def get_medicines():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM medicines LIMIT 10")
+    rows = cursor.fetchall()
+    conn.close()
+    return jsonify(rows)
+@bp.route('/')
+def home():
+    return "Backend is live "
