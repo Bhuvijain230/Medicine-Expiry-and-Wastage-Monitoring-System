a72a51ba613cc1932594d80ff5b0837b73997072
Harsh7450 <135258527+Harsh7450@users.noreply.github.com>
2025-07-22 09:05:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'email dalna yahapr'
+MAIL_PASSWORD = 'apppassword of Email'     
+
+
+TWILIO_ACCOUNT_SID = 'account number yaha pr'
+TWILIO_AUTH_TOKEN = 'authentication number yaha pr'
+TWILIO_PHONE_NUMBER = 'twilio ka number dalna hai'
